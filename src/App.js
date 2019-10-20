6f46ee426745169a981ee606dde2ef30d78981d1
Fbrito1 <luan.tecno@gmail.com>
2019-10-20 16:20:24 -0300

    routes configuration | project pages setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router } from 'react-router-dom';
+
+import Routes from './routes';
+import history from './services/history';
 
 function App() {
   return (
-    <div className="App" />
+    <Router history={history}>
+      <Routes />
+    </Router>
   );
 }
 
