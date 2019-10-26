a78858f76621249a9e275bf65244a936d44389da
brito <luan.tecno@gmail.com>
2019-10-26 20:43:28 -0300

    add auth module > reducer | action | sagas

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,27 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
 
-export default all([]);
+import api from '~/services/api';
+import { singInSuccess } from './actions';
+
+import history from '~/services/history';
+
+export function* singIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', {
+    email,
+    password,
+  });
+
+  const { token, user } = response.data;
+
+  if (!user.provider) {
+    console.tron.error('Usuário não é prestador');
+  }
+
+  yield put(singInSuccess(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SING_IN_REQUEST', singIn)]);
