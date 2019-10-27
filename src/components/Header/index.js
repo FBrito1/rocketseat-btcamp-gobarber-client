edf9c53451c7b611c1404bcc29ac8ea41222bce8
brito <luan.tecno@gmail.com>
2019-10-27 10:47:49 -0300

    Add Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container, Content, Profile } from './styles';
+
+import logoPurple from '~/assets/logo-purple.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logoPurple} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Fabio Brito</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.pngCopy to Clipboard"
+              alt="Fabio Brito"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
