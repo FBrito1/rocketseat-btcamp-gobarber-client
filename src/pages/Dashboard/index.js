98885fb899d828fd75dcaa512fd05b44a12ec4eb
brito <luan.tecno@gmail.com>
2019-10-27 18:10:48 -0300

    feat: switching date on the dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { format, subDays, addDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  function handlePrevDate() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDate() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button type="button" onClick={handlePrevDate}>
           <MdChevronLeft size={36} color="#FFF" />
         </button>
-        <strong>31 de Maio</strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDate}>
           <MdChevronRight size={36} color="#FFF" />
         </button>
       </header>
