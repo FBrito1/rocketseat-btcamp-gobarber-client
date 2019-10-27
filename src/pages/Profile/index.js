c48375043ddd60e54dcacd4e24ac9be65ee9c80e
brito <luan.tecno@gmail.com>
2019-10-27 15:30:40 -0300

    add profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,44 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const dispatch = useDispatch();
+
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -11,6 +11,10 @@ export default function user(state = INITIAL_STATE, action) {
       return produce(state, draft => {
         draft.profile = action.payload.user;
       });
+    case '@user/UPDATE_PROFILE_SUCCESS':
+      return produce(state, draft => {
+        draft.profile = action.payload.profile;
+      });
 
     default:
       return state;
