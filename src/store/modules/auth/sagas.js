58e68cb98433d71581542e997a5ba54f16c7944b
brito <luan.tecno@gmail.com>
2019-10-26 21:26:48 -0300

    auth loading

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,27 +1,31 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects';
 
 import api from '~/services/api';
-import { singInSuccess } from './actions';
+import { singInSuccess, singFailure } from './actions';
 
 import history from '~/services/history';
 
 export function* singIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('Usuário não é prestador');
+    if (!user.provider) {
+      console.tron.error('Usuário não é prestador');
+    }
+
+    yield put(singInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (e) {
+    yield put(singFailure());
   }
-
-  yield put(singInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SING_IN_REQUEST', singIn)]);
